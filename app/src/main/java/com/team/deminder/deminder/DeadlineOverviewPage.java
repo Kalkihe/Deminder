15693fb1e6260f23361a62a4fefb89b4ae20f262
DrNuenninger <tillmann@nuenninghoff.de>
2018-11-20 01:00:42 +0100

    Create some base Methods

--- app/src/main/java/com/team/deminder/deminder/DeadlineOverviewPage.java
+++ app/src/main/java/com/team/deminder/deminder/DeadlineOverviewPage.java
@@ -1,4 +1,35 @@
 package com.team.deminder.deminder;
 
-public class DeadlineOverviewPage {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.team.deminder.deminder.Containers.Deadline;
+
+public class DeadlineOverviewPage extends AppCompatActivity {
+    private final SettingsPage settingsPage;
+    private ManageDeadlinePage manageDeadlinePage;
+
+    public DeadlineOverviewPage() {
+        settingsPage = new SettingsPage();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void buildLayout(){
+
+        // nach drücken des buttons zur Erstellung einer neuen Deadline
+        // oder beim drücken auf eine deadline
+
+        manageDeadlinePage = new ManageDeadlinePage(new Deadline());
+        Intent intent = new Intent(this,ManageDeadlinePage.class);
+        intent.
+
+    }
+
+
 }
