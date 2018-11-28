5956be8a9e88319ef7f5956e26e7f3db9f2592d9
Tillmann Nuenninghoff <tillmann.nuenninghoff@cas.de>
2018-11-28 14:32:56 +0100

    Basic Methods for ManageDeadlinePage

--- app/src/main/java/com/team/deminder/deminder/ManageDeadlinePage.java
+++ app/src/main/java/com/team/deminder/deminder/ManageDeadlinePage.java
@@ -1,8 +1,11 @@
 package com.team.deminder.deminder;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 import com.team.deminder.deminder.Containers.Deadline;
 import com.team.deminder.deminder.Containers.Subtask;
@@ -24,18 +27,41 @@ public class ManageDeadlinePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manage_deadline_page);
+        storageManager = new StorageManager();
         isNewDeadline = true;
+
         intent = getIntent();
         deadline = (Deadline) intent.getSerializableExtra("deadline");
+
+        if (deadline != null) {
+            // Wenn eine deadline mitgeschickt wurde fülle alle componenten mit dessen Daten
+            fillLayoutComonents();
+            isNewDeadline = false;
+        }
+
+        initialiseLayoutComponents();
+    }
+
+
+    private void initialiseLayoutComponents() {
+        //TODO use findElementByViewID to bind all layout components to fields
+    }
+
+    private void fillLayoutComonents() {
+        //TODO fill in all the layou components with data from the existing deadline
     }
 
     private void saveDeadline() {
+        //TODO Auslesung aller layout komponenten und speichern in einer deadline
+        storageManager.saveDeadline(deadline);
+
         Intent returnIntent = new Intent();
         returnIntent.putExtra("deadline",deadline);
         returnIntent.putExtra("isNewDeadline",isNewDeadline);
         setResult(Activity.RESULT_OK,returnIntent);
         finish();
     }
+
     private void deleteDeadline() {
         if (isNewDeadline) {
             finish();
@@ -47,5 +73,15 @@ public class ManageDeadlinePage extends AppCompatActivity {
             setResult(Activity.RESULT_OK,returnIntent);
             finish();
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Ask user to confirm leaving the dialog and loosing all changes
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            // Is executed when the user leaves the ManageDeadlinePage
+            // without saving or deleting the deadline
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
