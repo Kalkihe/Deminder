8cbc24e85ea887129984df91b26755b41d6274ec
Luminca <leakecks@gmx.de>
2018-12-07 15:30:42 +0100

    Overview is shown, adding deadline is possible

--- app/src/main/java/com/team/deminder/deminder/customLayoutComponents/DeadlineLayoutWidget.java
+++ app/src/main/java/com/team/deminder/deminder/customLayoutComponents/DeadlineLayoutWidget.java
@@ -1,7 +1,41 @@
 package com.team.deminder.deminder.customLayoutComponents;
 
-public class DeadlineLayoutWidget {
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-    public DeadlineLayoutWidget() {
+import com.team.deminder.deminder.Containers.Deadline;
+import com.team.deminder.deminder.DeadlineOverviewPage;
+import com.team.deminder.deminder.ManageDeadlinePage;
+import com.team.deminder.deminder.R;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class DeadlineLayoutWidget {
+    private Context context;
+    private String deadlineName;
+    private String deadlineDate;
+    private LinearLayout linearLayout;
+
+    public DeadlineLayoutWidget(String deadlineName, String deadlineDate, Context context) {
+        this.deadlineName=deadlineName;
+        this.deadlineDate=deadlineDate;
+        this.context = context;
+    }
+
+    public LinearLayout getLayout(){
+        LinearLayout linearLayout= new LinearLayout(context);
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        TextView textDeadlineName = new TextView(context);
+        textDeadlineName.setText(deadlineName);
+        linearLayout.addView(textDeadlineName);
+        TextView textDeadlineDate = new TextView(context);
+        textDeadlineDate.setText(deadlineDate);
+        linearLayout.addView(textDeadlineDate);
+        return linearLayout;
     }
 }
