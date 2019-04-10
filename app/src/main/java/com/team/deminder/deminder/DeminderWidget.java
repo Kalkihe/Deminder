6c98814d8f9d8caafb79339071a0a9c86c054e2f
Tillmann Nuenninghoff <tillmann.nuenninghoff@cas.de>
2019-04-10 11:40:33 +0200

    Base Sceleton for Deminder Widget

---
+++ app/src/main/java/com/team/deminder/deminder/DeminderWidget.java
@@ -0,0 +1,13 @@
+package com.team.deminder.deminder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DeminderWidget extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
