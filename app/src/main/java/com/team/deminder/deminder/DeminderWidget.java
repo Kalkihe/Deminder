e26bef2b1d045e4e2ae36234a8b32b7accdabbc7
Tillmann Nuenninghoff <tillmann.nuenninghoff@cas.de>
2019-05-22 11:01:08 +0200

    WIP widget openable

--- app/src/main/java/com/team/deminder/deminder/DeminderWidget.java
+++ app/src/main/java/com/team/deminder/deminder/DeminderWidget.java
@@ -1,13 +1,35 @@
 package com.team.deminder.deminder;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 
-public class DeminderWidget extends BroadcastReceiver {
-
+public class DeminderWidget extends AppWidgetProvider {
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+        System.out.println("Thou am updateing");
 
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, DeadlineOverviewPage.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.deminder_widget_page);
+            views.setOnClickPendingIntent(R.id.rootLayout, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+            System.out.println("I am updateing");
+        }
     }
 }
