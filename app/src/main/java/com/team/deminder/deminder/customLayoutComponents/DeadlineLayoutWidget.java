572ef37925277f4c75d6d7c3aebd95843412ba91
DrNuenninger <tillmann@nuenninghoff.de>
2019-06-18 18:27:51 +0200

    Show subtask UC

--- app/src/main/java/com/team/deminder/deminder/customLayoutComponents/DeadlineLayoutWidget.java
+++ app/src/main/java/com/team/deminder/deminder/customLayoutComponents/DeadlineLayoutWidget.java
@@ -12,25 +12,31 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.team.deminder.deminder.Containers.Deadline;
+import com.team.deminder.deminder.Containers.Subtask;
 import com.team.deminder.deminder.DeadlineOverviewPage;
 import com.team.deminder.deminder.ManageDeadlinePage;
 import com.team.deminder.deminder.R;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class DeadlineLayoutWidget {
     private Context context;
-    private LinearLayout linearLayout;
+    private LinearLayout rootLayout;
     private Deadline deadline;
     private ImageButton imageButton;
+    private ArrayList<LinearLayout> subtaskWidgets;
 
     public DeadlineLayoutWidget(Deadline deadline, Context context, int deadlineID, Activity deadlineOverviewPage) {
         this.deadline = deadline;
         this.context = context;
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
-        linearLayout=  new LinearLayout(context);
+        rootLayout =  new LinearLayout(context);
+        rootLayout.setOrientation(LinearLayout.VERTICAL);
+
+        LinearLayout linearLayout=  new LinearLayout(context);
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
 
         LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(
@@ -75,12 +81,10 @@ public class DeadlineLayoutWidget {
         textDeadlineDate.setGravity(Gravity.RIGHT);
         linearLayout.addView(textDeadlineDate, textParams);
 
-
-/*
         imageButton = new ImageButton(context);
         imageButton.setBackgroundResource(R.drawable.ic_drop_down_grey);
         linearLayout.addView(imageButton);
-*/
+
 
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,13 +94,60 @@ public class DeadlineLayoutWidget {
                 deadlineOverviewPage.startActivityForResult(intent, deadlineID);
             }
         });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            boolean shown = false;
+
+            @Override
+            public void onClick(View v) {
+                if (shown) {
+                    for (int i = 0; i < subtaskWidgets.size(); i++) {
+                        subtaskWidgets.get(i).setVisibility(View.GONE);
+                        shown = false;
+                    }
+                } else {
+                    for (int i = 0; i < subtaskWidgets.size(); i++) {
+                        subtaskWidgets.get(i).setVisibility(View.VISIBLE);
+                        shown = true;
+                    }
+                }
+
+            }
+        });
+        rootLayout.addView(linearLayout);
+        subtaskWidgets = buildSubtaskList();
     }
 
     public LinearLayout getLayout(){
-        return linearLayout;
+        return rootLayout;
     }
 
     public Deadline getDeadline() {
         return deadline;
+    }
+
+    private ArrayList buildSubtaskList () {
+        ArrayList<Subtask> subtaskList = deadline.getSubtaskList();
+        ArrayList<LinearLayout> subtaskLayoutList = new ArrayList<>();
+
+        for (int i = 0; i < subtaskList.size() ; i++) {
+            LinearLayout subtaskLayout = new LinearLayout(context);
+            subtaskLayout.setOrientation(LinearLayout.HORIZONTAL);
+            subtaskLayout.setVisibility(View.GONE);
+
+            LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT,1f);
+            textParams.setMarginStart(50);
+
+            TextView textSubtaskName = new TextView(context);
+            textSubtaskName.setText(subtaskList.get(i).getSubtaskName());
+            textSubtaskName.setTextSize(18);
+
+            subtaskLayout.addView(textSubtaskName,textParams);
+
+            subtaskLayoutList.add(subtaskLayout);
+            rootLayout.addView(subtaskLayout);
+        }
+        return subtaskLayoutList;
     }
 }
